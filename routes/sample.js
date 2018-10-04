9600b1217c54098c614027910a06641245d607be
ahn <93immm@naver.com>
2018-10-04 19:21:15 +0900

    feat: add sample jwt

--- routes/sample.js
+++ routes/sample.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const accessTokenView = require('../views/sample/accessToken')
 const connectionTestView = require('../views/sample/connectionTest')
 const sampleView = require('../views/sample/sample')
 
+router.get('/accessToken', accessTokenView.accessToken)
 router.get('/connectionTest', connectionTestView.connectionTest)
 router.get('/', sampleView.sample)
 
