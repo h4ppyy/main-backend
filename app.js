a4729b7f9a70f47984c13692b18b681f578a00f7
ahn <93immm@naver.com>
2018-10-04 09:33:05 +0000

    feat: delete init sample code

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var sampleRouter = require('./routes/sample');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -16,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/sample', sampleRouter);
-app.use('/users', usersRouter);
 
 // error handler(404)
 app.use(function(req, res, next) {
